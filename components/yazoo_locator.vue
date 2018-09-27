894049515879bcb025c1d4edbe125a03f265c13a
Chris Zubak-Skees <chriszs@gmail.com>
2018-09-26 22:23:02 -0400

    Clean up Yazoo locator, convert dot to circle

--- components/yazoo_locator.vue
+++ components/yazoo_locator.vue
@@ -2,7 +2,7 @@
     <svg :viewBox="viewDim">
         <svg :width="width" :height="height">
             <path class="state" :d="state" />
-            <path class="county" :d="projected" />
+            <circle class="county" :cx="projected[0]" :cy="projected[1]" r="4" />
         </svg>
     </svg>
 </template>
@@ -12,7 +12,6 @@ import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
 import ms from '~/assets/ms_silhouette.json';
 import yazoo from '~/assets/yazoo_coords.csv';
-import yazooJson from '~/assets/yazoo_city.json';
 
 export default {
 
@@ -21,28 +20,21 @@ export default {
         let height = 600;
 
         let msShape = topojson.feature(ms, ms.objects.ms_silhouette);
-        let yazooPoint = topojson.feature(yazooJson, yazooJson.objects.yazoo_city)
-
-        console.log(yazooPoint)
 
         let projection = d3.geoTransverseMercator()
             .rotate([88 + 50 / 60, -29 - 30 / 60])
             .fitSize([width,height],msShape);
 
-// This isn't working for some reason
         let path = d3.geoPath()
             .projection(projection);
 
         let state = path(msShape.features[0]);
 
-        let coords = yazoo.split(',');
-        coords[0] = Number(coords[0]);
-        coords[1] = Number(coords[1]);
+        let coords = yazoo.split(',')
+                        .map(coord => +coord)
+                        .reverse();
 
         let projected = projection(coords);
-
-        projected = path(yazooPoint);
-        console.log(coords)
 
         let viewDim = "0 0 "+width.toString()+" "+height.toString()
         return {
@@ -51,15 +43,13 @@ export default {
             state,
             viewDim,
             projected
-//            lat,
-//            long
         }
     }
 };
 
 </script>
 
-<style>
+<style scoped>
 
 .state {
     fill: white;
