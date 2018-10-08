0e363abae83cf3721f41663cfabd73548d1fa862
Chris Zubak-Skees <chriszs@gmail.com>
2018-10-08 11:41:23 -0400

    Fix subscribe centering

--- components/Subscribe.vue
+++ components/Subscribe.vue
@@ -1,18 +1,18 @@
 <template>
-    <center>
     <!-- Begin Mailchimp Signup Form -->
-        <div class="mc_embed_signup">
-            <form action="https://publicintegrity.us4.list-manage.com/subscribe/post?u=8dc6eceed67f7f012462d0b12&amp;id=8d644d0a0e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
-                <div id="mc_embed_signup_scroll">
-                <label for="mce-EMAIL">Get alerted to the next installment of Abandoned in America</label><br>
-                <input type="email" value="" name="EMAIL" class="email form-control" id="mce-EMAIL" placeholder="email address" required>
-                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8dc6eceed67f7f012462d0b12_8d644d0a0e" tabindex="-1" value=""></div>
-                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-primary">
-                </div>
-            </form>
-        </div>
-    </center>
+    <div class="mc_embed_signup centralColumn">
+        <center>
+        <form action="https://publicintegrity.us4.list-manage.com/subscribe/post?u=8dc6eceed67f7f012462d0b12&amp;id=8d644d0a0e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+            <label for="mce-EMAIL">Get alerted to the next installment of Abandoned in America</label><br>
+            <input type="email" value="" name="EMAIL" class="email form-control" id="mce-EMAIL" placeholder="email address" required>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8dc6eceed67f7f012462d0b12_8d644d0a0e" tabindex="-1" value=""></div>
+            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-primary">
+            </div>
+        </form>
+        </center>
+    </div>
 
 <!--End mc_embed_signup-->
 </template>
