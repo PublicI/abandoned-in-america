fce5520dee70252558017da4612838d32ec40ba4
Chris Zubak-Skees <chriszs@gmail.com>
2018-10-13 13:38:02 -0400

    Revert those

--- components/FresnoThematic.vue
+++ components/FresnoThematic.vue
@@ -47,7 +47,7 @@ export default {
 
     data() {
         let width = 800;
-        let height = 900;
+        let height = 800;
         let fresnoShapes = topojson.feature(fresno, fresno.objects.clipped_tract_data);
         let roadPaths = topojson.feature(roads, roads.objects.highways);
         let swOutline = topojson.feature(borders, borders.objects.sw);
@@ -146,7 +146,7 @@ export default {
  top: 0;
 }
 .fresnoThematic.bigger {
-    max-width: 700px;
+    max-width: 800px;
     margin-left: auto;
     margin-right: auto;
 }
@@ -269,7 +269,7 @@ export default {
     font-family: "nimbus-sans",sans-serif;
 }
 
-@media (max-width: 550px) {
+@media (max-width: 580px) {
     .fresnoThematic.left {
         width: 297px;
         float: none;
