b71a912b158ddf62c0f950e00f22d0caf22d18ca
Chris Zubak-Skees <chriszs@gmail.com>
2018-10-13 13:00:27 -0400

    Mute the video by default

--- components/LoopingVideo.vue
+++ components/LoopingVideo.vue
@@ -1,14 +1,27 @@
 <template>
     <div class="bigger">
-        <video style="width: 100%;height: auto" autoplay loop muted playsinline>
+        <video style="width: 100%;height: auto" id="loopingVideo" loop playsinline muted>
           <source :src="data.location" type="video/mp4">
         </video>
     </div>
 </template>
 
 <script>
+import inViewport from 'vue-in-viewport-mixin';
+
 export default {
-    props: ['data']
+    props: ['data'],
+    mixins: [ inViewport ],
+    watch: {
+        'inViewport.now': visible => {
+            if (visible) {
+                document.getElementById('loopingVideo').play();
+            }
+            else {
+                document.getElementById('loopingVideo').pause();
+            }
+        }
+    }
 };
 </script>
 
