0a2fe3405a95afa96c7080ae74b38eb4e220009d
Chris Zubak-Skees <chriszs@gmail.com>
2018-10-16 18:08:04 -0400

    Update soundcloud embed

--- components/Soundcloud.vue
+++ components/Soundcloud.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="centralColumn">
-        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/513014469%3Fsecret_token%3Ds-kYtNd&color=%232314f0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/515355231%3Fsecret_token%3Ds-bQNJr&color=%232314f0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
     </div>
 </template>
 
