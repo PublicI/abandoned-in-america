05d8be3fdfa936fd3de05d1ce9586523fbc68262
Chris Zubak-Skees <chriszs@gmail.com>
2018-10-17 13:01:14 -0400

    Edit Yazoo locator

--- components/YazooLocator.vue
+++ components/YazooLocator.vue
@@ -1,10 +1,14 @@
 <template>
-    <svg :viewBox="'0 0 '+width+' '+height">
-        <svg :width="width" :height="height">
-            <path class="state" :d="state" />
-            <circle class="city" :cx="projected[0]" :cy="projected[1]" r="10" />
-        </svg>
-    </svg>
+    <div>
+        <div class="scaling-svg-container">
+            <svg :viewBox="'0 0 '+width+' '+height" class="scaling-svg">
+                <svg :width="width" :height="height">
+                    <path class="state" :d="state" />
+                    <circle class="city" :cx="projected[0]" :cy="projected[1]" r="10" />
+                </svg>
+            </svg>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -49,14 +53,31 @@ export default {
 
 <style scoped>
 
+.scaling-svg-container {
+ position: relative; 
+ height: 0; 
+ width: 100%; 
+ padding: 0;
+ padding-bottom: 130%; 
+ /* override this inline for aspect ratio other than square */
+}
+.scaling-svg {
+ position: absolute; 
+ height: 100%; 
+ width: 100%; 
+ left: 0; 
+ top: 0;
+}
 .state {
-    fill: white;
+    fill: rgb(220,220,220);
     stroke: grey;
-    stroke-width: 2px;
+    stroke-width: 0;
 }
 
 .city {
-    fill: red;   
+    fill: #E74C3C;
+    stroke-width: 5px;
+    stroke: white;   
 }
 
 </style>
