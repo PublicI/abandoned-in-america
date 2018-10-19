63f861f7cbef0e88e12b00330b0a5e301f42ad6b
Chris Zubak-Skees <chriszs@gmail.com>
2018-10-18 23:20:36 -0400

    Tweak slideshow style

--- components/Slideshow.vue
+++ components/Slideshow.vue
@@ -3,9 +3,9 @@
   <div v-swiper:mySwiper="swiperOption">
     <div class="swiper-wrapper">
         <div class="swiper-slide" v-for="slide in doc.slides">
-            <img :src="encodeURI(base + slide.location.replace('img','resized').replace(/\.(jpg|png)$/,'-2880.$1'))" :srcset="encodeURI(base + slide.location.replace('img','resized').replace(/\.(jpg|png)$/,'-2880.$1')) + '  2880w,' + base + encodeURI(slide.location.replace('img','resized').replace(/\.(jpg|png)$/,'-1440.$1')) + '  1440w,' + base + encodeURI(slide.location.replace('img','resized').replace(/\.(jpg|png)$/,'-720.$1')) + '   720w'" style="width:100%;height: auto">
+            <img :src="encodeURI(base + slide.location.replace('img','resized').replace(/\.(jpg|png)$/,'-2880.$1'))" :srcset="encodeURI(base + slide.location.replace('img','resized').replace(/\.(jpg|png)$/,'-2880.$1')) + '  2880w,' + base + encodeURI(slide.location.replace('img','resized').replace(/\.(jpg|png)$/,'-1440.$1')) + '  1440w,' + base + encodeURI(slide.location.replace('img','resized').replace(/\.(jpg|png)$/,'-720.$1')) + '   720w'" class="slideshowImage">
 
-                <div class="cutline"><span v-html="'<p>' + slide.cutline.replace(/\n\W*\n/g,'</p>\n<p>') + '</p>'"></span><br><span class="credit" v-html="slide.credit"></span></div>
+            <div class="cutline"><span v-html="'<p>' + slide.cutline.replace(/\n\W*\n/g,'</p>\n<p>') + '</p>'"></span><span class="credit" v-html="slide.credit"></span></div>
         </div>
     </div>
     <div class="swiper-button-prev" slot="button-prev"></div>
@@ -22,6 +22,7 @@ export default {
     data () {
       return {
         swiperOption: {
+            autoHeight: true,
             pagination: {
                 clickable: true,
                 el: '.swiper-pagination'
@@ -49,8 +50,8 @@ export default {
 </script>
 
 <style>
-.slideshow {
-    max-width: 800px;
+.swiper-container {
+    max-width: 1000px;
     margin-left: auto;
     margin-right: auto;
 }
@@ -63,7 +64,8 @@ export default {
     line-height: 18px;
     font-size: 15px;
     color: rgb(140,140,140);
-    padding-bottom: 10px;
+    color: white;
+    padding-bottom: 0px;
     padding-top: 5px;
 }
 
@@ -78,7 +80,7 @@ export default {
 .slideshow .credit {
     font-size: 13px;
     line-height: 16px;
-    color: rgb(160,160,160);
+    color: rgb(200,200,200);
     padding-left: 15px;
 }
 
@@ -91,8 +93,13 @@ export default {
     padding-left: 0;
 }
 
+.slideshow .swiper-pagination-bullet {
+    background: white;
+    opacity: 0.3;
+}
 .slideshow .swiper-pagination-bullet-active {
-    background: rgb(50,50,50);
+    background: white;
+    opacity: 1;
 }
 .swiper-button-next, .swiper-container-rtl .swiper-button-prev {
     background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M27%2C22L27%2C22L5%2C44l-2.1-2.1L22.8%2C22L2.9%2C2.1L5%2C0L27%2C22L27%2C22z'%20fill%3D'%23FFFFFF'%2F%3E%3C%2Fsvg%3E");
@@ -102,17 +109,35 @@ export default {
 }
 
 .swiper-button-next, .swiper-container-rtl .swiper-button-prev, .swiper-button-prev, .swiper-container-rtl .swiper-button-next {
-    top: 45%;
+    top: 313px;
+    -webkit-tap-highlight-color: transparent;
+}
+
+.slideshow {
+    background-color: rgb(30,30,30);
+    margin-bottom: 20px;
+    padding-bottom: 10px;
+}
+
+.slideshowImage {
+    width:auto;
+    max-height: 600px;
+    margin:0px auto;
+    display: table;
 }
 
 @media (max-width: 900px) {
     .swiper-button-next, .swiper-container-rtl .swiper-button-prev, .swiper-button-prev, .swiper-container-rtl .swiper-button-next {
-        top: 35%;
+        top: 160px;
+    }
+
+    .slideshowImage {
+        max-height: 300px;
     }
 
     .slideshow .cutline {
-        padding-left: 3px;
-        padding-right: 3px;
+        padding-left: 5px;
+        padding-right: 5px;
     }
 }
 </style>
